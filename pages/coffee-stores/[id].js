5219fe997ecb14f8a9f5b2755507294a19456638
And4Web <anands2807@gmail.com>
2022-05-31 12:50:45 +0530

    added a nextjs Link component

--- pages/coffee-stores/[id].js
+++ pages/coffee-stores/[id].js
@@ -1,9 +1,16 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const CoffeeStores = () => {
   const router = useRouter();
   console.log("router", router);
-  return <div> Coffee stores: {router.query.id}</div>;
+  return (
+    <div>
+      {" "}
+      Coffee stores: {router.query.id}
+      <Link href="/">|||Back to home|||</Link>
+    </div>
+  );
 };
 
 export default CoffeeStores;
