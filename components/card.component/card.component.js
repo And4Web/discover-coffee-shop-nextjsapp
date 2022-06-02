b94ee53e6a730f26a0603aec2ef49a5e0162baa9
And4Web <anands2807@gmail.com>
2022-06-02 12:42:10 +0530

    In the card component glassmorphism added, used classnames library to add more classes.

--- components/card.component/card.component.js
+++ components/card.component/card.component.js
@@ -1,13 +1,28 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./card.component.module.css";
+
+import cls from "classnames";
 
 const Card = (props) => {
   const { name, href, imgUrl } = props;
   return (
     <Link href={href}>
-      <a>
-        <h2>{name}</h2>
-        <Image src={imgUrl} alt={name} width={260} height={160} />
+      <a className={styles.cardLink}>
+        <div className={cls("glass", styles.container)}>
+          <div className={styles.cardHeaderWrapper}>
+            <h2 className={styles.cardHeader}>{name}</h2>
+          </div>
+          <div className={styles.cardImageWrapper}>
+            <Image
+              className={styles.cardImage}
+              src={imgUrl}
+              alt={name}
+              width={260}
+              height={160}
+            />
+          </div>
+        </div>
       </a>
     </Link>
   );
