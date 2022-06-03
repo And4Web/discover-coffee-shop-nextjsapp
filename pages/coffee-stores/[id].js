ade25993db3198f407acbf0cf3b1990aace3b2f1
And4Web <anands2807@gmail.com>
2022-06-03 09:14:59 +0530

    learned getStaticProps() getStaticPaths() and fallback in this function

--- pages/coffee-stores/[id].js
+++ pages/coffee-stores/[id].js
@@ -5,32 +5,34 @@ import coffeeStoresData from "../../data/coffee-stores.json";
 
 export function getStaticProps(staticProps) {
   const { params } = staticProps;
+  // console.log("params: ", params);
   return {
     props: {
-      coffeStores: coffeeStoresData.find((coffeeStore) => coffeeStore.id === 0),
+      coffeStores: coffeeStoresData.find(
+        (coffeeStore) => coffeeStore.id.toString() === params.id
+      ),
     },
   };
 }
 
 export function getStaticPaths() {
   return {
-    paths: [
-      {
-        paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
-      },
-    ],
-    fallback: true,
+    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    fallback: false,
   };
 }
 
-const CoffeeStores = () => {
+const CoffeeStores = (props) => {
   const router = useRouter();
-  console.log("router", router);
+  // console.log("router", router);
+  // console.log("props", props);
   return (
     <div>
       {" "}
       Coffee stores: {router.query.id}
-      {/* <Link href="/coffee-stores/dynamic">|||to dynamic|||</Link> */}
+      <Link href="/coffee-stores/dynamic">|||to dynamic|||</Link>
+      <p>{props.coffeStores.address}</p>
+      <p>{props.coffeStores.name}</p>
     </div>
   );
 };
