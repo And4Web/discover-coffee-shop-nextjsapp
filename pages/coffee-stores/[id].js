1b99771f69142a296cb4139d1513251c71d11e05
And4Web <anands2807@gmail.com>
2022-06-03 09:35:25 +0530

    if fallback is true, and page doesn't have a route in getStaticPaths() then use the router property isFallback() to fetch the data and meanwhile render Loading...

--- pages/coffee-stores/[id].js
+++ pages/coffee-stores/[id].js
@@ -18,12 +18,16 @@ export function getStaticProps(staticProps) {
 export function getStaticPaths() {
   return {
     paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
-    fallback: false,
+    fallback: true,
   };
 }
 
 const CoffeeStores = (props) => {
   const router = useRouter();
+  if (router.isFallback) {
+    return <h1>Loading...</h1>;
+  }
+  const { name, address } = props.coffeStores;
   // console.log("router", router);
   // console.log("props", props);
   return (
@@ -31,8 +35,8 @@ const CoffeeStores = (props) => {
       {" "}
       Coffee stores: {router.query.id}
       <Link href="/coffee-stores/dynamic">|||to dynamic|||</Link>
-      <p>{props.coffeStores.address}</p>
-      <p>{props.coffeStores.name}</p>
+      <p>{address}</p>
+      <p>{name}</p>
     </div>
   );
 };
