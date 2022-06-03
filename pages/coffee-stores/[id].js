8dc60dc57abaa148922e27d748e5f03c850cc000
And4Web <anands2807@gmail.com>
2022-06-03 08:58:49 +0530

    used getStaticPaths() for dynamic page prerendering alongwith getStaticProps() with condition

--- pages/coffee-stores/[id].js
+++ pages/coffee-stores/[id].js
@@ -1,6 +1,28 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 
+import coffeeStoresData from "../../data/coffee-stores.json";
+
+export function getStaticProps(staticProps) {
+  const { params } = staticProps;
+  return {
+    props: {
+      coffeStores: coffeeStoresData.find((coffeeStore) => coffeeStore.id === 0),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [
+      {
+        paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+      },
+    ],
+    fallback: true,
+  };
+}
+
 const CoffeeStores = () => {
   const router = useRouter();
   console.log("router", router);
