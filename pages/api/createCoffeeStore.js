c81d99d758871ac4f61953c609154da160669520
And4Web <anands2807@gmail.com>
2023-03-03 14:45:53 +0530

    took data from client request body and stored in the table

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -5,22 +5,54 @@ const table = base("Coffee-Stores");
 
 console.log("airtable table:===>", table)
 
-const createCoffeeStore = async (req, res) => {
-  if(req.method === "POST"){
-    //find a record:
-    const findCoffeeStoreRecords = await table.select({
-      filterByFormula: `id="0"`,
-    }).firstPage();
+const createCoffeeStore = async (req, res) => {  
+  try {
+    const {id, name, address, neighborhood, rating, imgUrl} = req.body;
+    if(req.method === "POST"){
+      //find a record:
+      const findCoffeeStoreRecords = await table.select({
+        filterByFormula: `id=${id}`,
+      }).firstPage();
+  
+      console.log(findCoffeeStoreRecords);
 
-    console.log(findCoffeeStoreRecords);
-    if(findCoffeeStoreRecords.length !== 0){
-      res.json({findCoffeeStoreRecords})
-    }else{
-      //create a record:
-      res.json({message: "create record..."})
+      if(findCoffeeStoreRecords.length !== 0){
+        const records = findCoffeeStoreRecords.map(record=>{
+          return {
+            ...record.fields
+          }
+        })
+        res.json(records);
+      }else{
+        //create a record:
+        const createRecord = await table.create([
+          {
+            fields: {
+              id,
+              name,
+              address,
+              neighborhood,
+              rating,
+              imgUrl
+            }
+          }
+        ])
+
+        const records = createRecord.map(record=>{
+          return {
+            ...record.fields
+          }
+        })
+        
+        res.json({message: "create record...", records})
+      }
+  
     }
-
+  } catch (error) {
+    console.error("Error finding Stores: ", error.message)
+    res.status(500).json({Error: error.message})
   }
+  
 }
 
 export default createCoffeeStore;
